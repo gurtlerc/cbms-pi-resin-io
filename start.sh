7123c029ce6a2fc1ec10c31f095155465079b3d6
Chris Gurtler <gurtlerc@gmail.com>
2016-05-09 10:47:48 +1000

    store all application files in the /data folder

--- start.sh
+++ start.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-# /bin/systemctl daemon-reload 
-# /bin/systemctl enable grafana-server
-# /bin/systemctl start grafana-server
+GRAFANA_DIR /data/grafana
+INFUXDB_DIR /data/influxdb
+CBMS_DIR /data/cbms
+
+# Make and chown influxdb data directories
+if [ ! -f $INFLUXDB_DIR ]
+then
+	mkdir $INFLUXDB_DIR
+	cke and chown grafana data directories
+if [ ! -f $GRAFANA_DIR ]
+then
+        mkdir $GRAFANA_DIR
+        chown grafana:grafana $GRAFANA_DIR
+        cp
+fi
+hown influxdb:influxdb $INFLUXDB_DIR
+fi
+
+# Make and chown cbms data directories
+if [ ! -f $CBMS_DIR ]
+then
+	mv /var/cbms /data/cbms
+fi
 
